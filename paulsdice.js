c4a3fcecc79f6aedd3ddd15826f4409858078ec6
Paul Kaefer <paul.kaefer@gmail.com>
2012-11-09 15:31:39 -0600

    Testing array creation, sorting, and reversing.

--- paulsdice.js
+++ paulsdice.js
@@ -2,8 +2,10 @@
 /*
  * Sources:
  *
+ * Arrays in JavaScript: http://www.virtualmv.com/wiki/index.php?title=JavaScript:Arrays
  * Math.random() function: http://www.javascriptkit.com/javatutors/randomnum.shtml
  * Using "var" vs. not using "var": http://stackoverflow.com/questions/1470488/difference-between-using-var-and-not-using-var-in-javascript
+ *
  */
 
 function roll(number, dieValue)
@@ -18,3 +20,35 @@ function roll(number, dieValue)
     return rollTotal;
 }
 
+function test()
+{
+    document.write("hello");
+    document.write("<br>a");
+}
+
+function testArrays()
+{
+    // Debugging:
+    //document.write("a");
+    var dice_array = new Array(4);
+    var temp = 0;
+    for (i=0; i<4; i++)
+    {
+        dice_array[i] = (Math.floor(Math.random()*6 + 1));
+        document.write("<br>dice_array["+i+"]= "+dice_array[i]);
+    }
+    dice_array = dice_array.sort();
+    document.write("<br><br>Sorted:<br><br>");
+    for (i=0; i<4; i++)
+    {
+        document.write("<br>dicearray["+i+"]= "+dice_array[i]);
+    }
+    dice_array = dice_array.reverse();
+    document.write("<br><br>Reversed:<br><br>");
+    for (i=0; i<4; i++)
+    {
+        document.write("<br>dice_array["+i+"]= "+dice_array[i]);
+    }
+    // Debugging:
+    //document.write("b");
+}
