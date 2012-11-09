47db0494f22f876cfaa0f9930598d1e9be525320
Paul Kaefer <paul.kaefer@gmail.com>
2012-11-09 17:27:58 -0600

    Added clearOutputBox() function and roll n*dx function.

--- paulsdice.js
+++ paulsdice.js
@@ -7,17 +7,18 @@
  * Checkbox default value and return value: http://www.echoecho.com/htmlforms09.htm
  * DND dice roller: http://www.wizards.com/dnd/dice/dice.htm
  * Math.random() function: http://www.javascriptkit.com/javatutors/randomnum.shtml
+ * Simple HTML buttons: http://www.w3.org/TR/html401/interact/forms.html#h-17.4.2
  * Using "var" vs. not using "var": http://stackoverflow.com/questions/1470488/difference-between-using-var-and-not-using-var-in-javascript
  *
  */
 
-function roll(number, dieValue)
+function rollDice(number, dieValue)
 {
     var rollTotal = 0;
     
     for (i=0; i<number; i++)
     {
-        rollTotal += Math.floor(Math.random()*dieValue + 1);
+        rollTotal = rollTotal + Math.floor(Math.random()*dieValue + 1);
     }
     
     return rollTotal;
@@ -48,4 +49,43 @@ function testArrays()
     }
     // Debugging:
     //document.write("b");
+}
+
+function test()
+{
+    value1 = document.form1.numberOfDice.value;
+    document.form1.output.value = value1;
+}
+
+function rollNdX()
+{
+    number = parseInt(document.form1.numberOfDice.value);
+    dieValue = parseInt(document.form1.dieValue.value);
+    
+    currentRoll = 0;
+    total = 0;
+    outputText = "";
+    
+    clearOutputBox();
+    
+    for (i=0; i < number; i++)
+    {
+        currentRoll = Math.floor(Math.random()*dieValue + 1);
+        outputText = outputText + currentRoll.toString();
+        if (i < (number - 1))
+        {
+            outputText += ",";
+        }
+        total = total + currentRoll;
+    }
+    
+    outputText = "Rolled "+number.toString()+"d"+dieValue.toString()+"\nRolls: "+outputText+"\n--------------------\nTotal: "+total.toString();
+    
+    document.form1.output.value = outputText;
+
+}
+
+function clearOutputBox()
+{
+    document.form1.output.value = "";
 }
